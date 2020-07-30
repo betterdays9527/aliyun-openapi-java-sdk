59fff4ce4c14cb11bcf8e3428b37483e873c7075
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-07-30 11:20:27 +0800

    Improve MapUtils 1. Optimizing Judgment Conditions 2. Improve test case

--- aliyun-java-sdk-core/src/test/java/com/aliyuncs/utils/MapUtilsTest.java
+++ aliyun-java-sdk-core/src/test/java/com/aliyuncs/utils/MapUtilsTest.java
@@ -11,38 +11,6 @@ import java.util.Map;
 
 
 public class MapUtilsTest {
-    @Test
-    public void setListTest() throws Exception {
-        MapUtils mapUtils = new MapUtils();
-        Class clazz = MapUtils.class;
-        Method setList = clazz.getDeclaredMethod("setList", List.class, int.class, String.class, String.class);
-        setList.setAccessible(true);
-        List<Map<Object, Object>> list = null;
-        list = (List<Map<Object, Object>>) setList.invoke(mapUtils, list, 0, "test", "test");
-        Assert.assertEquals("test", list.get(0).get("test"));
-
-        Map<Object, Object> map = new HashMap<Object, Object>();
-        map.put("test", "test");
-        list = new ArrayList<Map<Object, Object>>();
-        list.add(map);
-        list = (List<Map<Object, Object>>) setList.invoke(mapUtils, list, 0, "test", "test");
-        Assert.assertEquals("test", list.get(0).get("test"));
-    }
-
-    @Test
-    public void setMapTest() throws Exception {
-        MapUtils mapUtils = new MapUtils();
-        Class clazz = MapUtils.class;
-        Method setMap = clazz.getDeclaredMethod("setMap", Map.class, String.class, String.class);
-        setMap.setAccessible(true);
-        Map<Object, Object> map = null;
-        map = (Map<Object, Object>) setMap.invoke(mapUtils, map, "test", "test");
-        Assert.assertEquals("test", map.get("test"));
-
-        map.put("test", new HashMap<Object, Object>());
-        map = (Map<Object, Object>) setMap.invoke(mapUtils, map, "test.test1", "test");
-        Assert.assertEquals("{test1=test}", map.get("test").toString());
-    }
 
     @Test
     public void convertMapToListMapTest() {
@@ -79,10 +47,22 @@ public class MapUtilsTest {
         testMap.put("DescribeAntChainNodesResponse.RequestId", "test");
         testMap.put("DescribeAntChainNodesResponse.Result[0].endpoints[0].ip", "test");
         testMap.put("DescribeAntChainNodesResponse.Result[3].nodeState", "test");
+        testMap.put("DescribeAntChainNodesResponse.List[0].key", "ListKey");
+        testMap.put("DescribeAntChainNodesResponse.List[0].map.key.List[0]", "ListMapKeyList");
         MapUtils mapUtils = new MapUtils();
         List<Map<Object, Object>> list = mapUtils.convertMapToListMap(testMap, "DescribeAntChainNodesResponse.Result");
         Assert.assertEquals("test", list.get(0).get("nodeState"));
         Assert.assertTrue(list.get(0).get("endpoints") instanceof List);
+
+        List<Map<Object, Object>> list2 = mapUtils.convertMapToListMap(testMap, "DescribeAntChainNodesResponse.List");
+        Assert.assertEquals(list2.get(0).get("key"), "ListKey");
+        Assert.assertTrue(list2.get(0).get("map") instanceof Map);
+        Map m = (Map) list2.get(0).get("map");
+        Assert.assertTrue(m.get("key") instanceof Map);
+        Map mm = (Map) m.get("key");
+        Assert.assertTrue(mm.get("List") instanceof List);
+        List<String> l = (List) mm.get("List");
+        Assert.assertEquals(l.get(0), "ListMapKeyList");
     }
 
     @Test
